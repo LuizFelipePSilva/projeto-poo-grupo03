66244e66858ba2b7e9104ea99036bb01e4783f19
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-07-22 10:27:33 -0300

    refactor: adjustment new params in methods, now use interfaces, not proprietys always

--- Projeto-poo/src/main/java/br/com/grupo03/projetopoo/model/dao/interfaces/IUsuarioDAO.java
+++ Projeto-poo/src/main/java/br/com/grupo03/projetopoo/model/dao/interfaces/IUsuarioDAO.java
@@ -1,13 +1,13 @@
 package br.com.grupo03.projetopoo.model.dao.interfaces;
 
-import br.com.grupo03.projetopoo.model.entity.Usuario;
+import br.com.grupo03.projetopoo.model.entity.interfaces.IUsuario;
 import java.util.List;
 
 public interface IUsuarioDAO {
-    Usuario save(Usuario user);
-    Usuario findById(Long id);
-    Usuario findByLogin(String login);
-    Usuario findByName(String name);
-    void remove(Usuario user);
-    List<Usuario> findAllUser();
+    IUsuario save(IUsuario user);
+    IUsuario findById(Long id);
+    IUsuario findByLogin(String login);
+    IUsuario findByName(String name);
+    void remove(IUsuario user);
+    List<IUsuario> findAllUser();
 }

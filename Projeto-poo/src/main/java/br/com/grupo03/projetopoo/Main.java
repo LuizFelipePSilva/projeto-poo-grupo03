82745eedb35ebca88a49b93f779b726d01112d58
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-07-30 13:30:53 -0300

    feat: add Main method for use in project

---
+++ Projeto-poo/src/main/java/br/com/grupo03/projetopoo/Main.java
@@ -0,0 +1,10 @@
+package br.com.grupo03.projetopoo;
+
+import br.com.grupo03.projetopoo.views.TelaLogin;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(TelaLogin.class, args);
+    }
+}

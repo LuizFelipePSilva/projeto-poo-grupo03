2b5a63a00a7bf8bb6726c4f6c01d9076a155ffed
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-07-30 15:37:00 -0300

    refactor: now just User.GERENTE is allowed to use Button and VBox admin

--- Projeto-poo/src/main/java/br/com/grupo03/projetopoo/Controller/TelaPrincipalController.java
+++ Projeto-poo/src/main/java/br/com/grupo03/projetopoo/Controller/TelaPrincipalController.java
@@ -1,12 +1,39 @@
 package br.com.grupo03.projetopoo.Controller;
 
+import br.com.grupo03.projetopoo.model.service.UsuarioService;
 import br.com.grupo03.projetopoo.views.TelaLogin;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
-import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class TelaPrincipalController {
+public class TelaPrincipalController implements Initializable {
+    @FXML
+    private Button btnAdmin;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        boolean isGerente;
+        try {
+            UsuarioService.checkGerente(); // Tenta executar a verificação de gerente
+            isGerente = true;
+        } catch (SecurityException e) {
+            isGerente = false; // Se der erro de segurança, não é gerente
+        }
+
+        // Define a visibilidade dos componentes com base na permissão
+        btnAdmin.setVisible(isGerente);
+        btnAdmin.setManaged(isGerente); // Garante que o botão não ocupe espaço se estiver invisível
+
+        boxAdmin.setVisible(isGerente);
+        boxAdmin.setManaged(isGerente); // Garante que o VBox não ocupe espaço se estiver invisível
+    }
+
+    @FXML
+    private VBox boxAdmin;
     public void sair(){
         TelaLogin.telaLogin();
     }
